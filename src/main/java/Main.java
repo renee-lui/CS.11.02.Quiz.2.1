1087126f88e9521d286e4d621b07645f740a4ba6
renee-lui <reneerllui@gmail.com>
2024-03-06 08:13:32 +0800

    committed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,54 +1,30 @@
 public class Main {
-
     public static void main(String[] args) {
+        int resultOne = add(5, 10);
+        System.out.println(resultOne);
 
-        // call the first add method and store the result into a variable called resultOne.
+        String resultTwo = greeting("Alice");
+        System.out.println(resultTwo);
 
-        // print resultOne.
+        int resultThree = add(2, 4, 6, 8);
+        System.out.println(resultThree);
 
-        // call the greeting method and store the result into a variable called resultTwo.
-
-        // print resultTwo.
-
-        // call the second add method and store the result into a variable called resultThree.
-
-        // print resultThree.
-
-        // call the printCapitalised method.
-
+        printCapitalised("hello world");
     }
 
-    /**
-     * add
-     * @param numOne - an integer
-     * @param numTwo - an integer
-     * @return the sum of numOne and numTwo (an integer)
-     */
-
-
-    /**
-     * greeting
-     * @param name - the name of a person (a String)
-     * @return a greeting in the format of "Bonjour, [name]!" (a String)
-     */
-
-
-    /**
-     * add
-     * @param numOne - an integer
-     * @param numTwo - an integer
-     * @param numThree - an integer
-     * @param numFour - an integer
-     * @return the sum of numOne, numTwo, numThree, and numFour (an integer)
-     */
-
-
-    /**
-     * printCapitalised
-     * @param toBePrinted - a String that the user wishes to be printed in all caps.
-     */
-
-
-
-
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+    public static String greeting(String name) {
+        return "Bonjour, " + name + "!";
+    }
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+        int sum1 = add(numOne, numTwo);
+        int sum2 = add(sum1, numThree);
+        int sum3 = add(sum2, numFour);
+        return sum3;
+    }
+    public static void printCapitalised(String toBePrinted) {
+        System.out.println(toBePrinted.toUpperCase());
+    }
 }
